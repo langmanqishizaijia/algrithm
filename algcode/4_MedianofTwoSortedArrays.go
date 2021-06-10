724a23e647dca6133b53fbd95960c2a2293d5f06
shuchaowang <shuchaowang@tencent.com>
2021-06-11 00:03:30 +0800

    --story=2021061022 查找两个有序数组的中位数

--- algcode/4_MedianofTwoSortedArrays.go
+++ algcode/4_MedianofTwoSortedArrays.go
@@ -1,80 +1,53 @@
 package algcode
 
-/*
-
-There are two sorted arrays nums1 and nums2 of size m and n respectively.
-
-Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
-
-You may assume nums1 and nums2 cannot be both empty.
-
-Example 1:
-
-nums1 = [1, 3]
-nums2 = [2]
-
-The median is 2.0
-Example 2:
-
-nums1 = [1, 2]
-nums2 = [3, 4]
-
-The median is (2 + 3)/2 = 2.5
-*/
-
-//1
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	/**
+	  1. 计算两个数组的长度
+	  2. 根据两个长度判断是取1个还是2个
+	  3. 同时遍历两个数组
+	  4. 找到要找的值后计算中位数
+	*/
 
-	len1, len2 := len(nums1), len(nums2)
+	l1, l2 := len(nums1), len(nums2)
 
-	maxlen := int((len1+len2)/2) + 1
-
-	lena := 0
-	lenb := 0
-	slice := make([]int, 0)
-	for i := 0; i < maxlen; i++ {
-
-		if lena < len1 && lenb < len2 {
-			if nums1[lena] < nums2[lenb] {
-				slice = append(slice, nums1[lena])
-				lena++
-			} else {
-				slice = append(slice, nums2[lenb])
-				lenb++
-			}
-		} else if lena < len1 {
-			slice = append(slice, nums1[lena])
-			lena++
-		} else {
-			slice = append(slice, nums2[lenb])
-			lenb++
-		}
-	}
-
-	if (len1+len2)%2 == 0 {
-		var tmp float64 = float64(slice[(len1+len2)/2] + slice[(len1+len2)/2-1])
-		return float64(tmp / 2)
-	} else {
-		return float64(slice[(len1+len2)/2])
+	if (l1+l2)&1 == 0 { // 偶数个
+		k := (l1 + l2) >> 1
+		//取 第 k 和 第 k+1
+		return (getK(nums1, nums2, k) + getK(nums1, nums2, k+1)) / 2
+	} else { //
+		k := (l1 + l2 + 1) >> 1
+		// 取第 k 个
+		return getK(nums1, nums2, k)
 	}
 
 }
 
-//2
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-
-	slice := make([]int, len(nums1)+len(nums2))
-	copy(slice, nums1)
-	copy(slice[len(nums1):], nums2)
-
-	sort.Ints(slice)
-
-	lenth := len(slice)
-	if lenth%2 == 0 {
-		fmt.Println("%+v", slice)
-		return (float64)(slice[lenth/2]+slice[(lenth-1)/2]) / 2
-	} else {
-		return float64(slice[lenth/2])
+func getK(nums1, nums2 []int, k int) float64 {
+	if len(nums1) == 0 {
+		return float64(nums2[k-1])
 	}
-
+	if len(nums2) == 0 {
+		return float64(nums1[k-1])
+	}
+	l1, l2 := 0, 0
+	ret := nums1[0]
+	for k > 0 {
+		if l1 < len(nums1) && l2 < len(nums2) {
+			if nums1[l1] < nums2[l2] {
+				ret = nums1[l1]
+				l1++
+			} else {
+				ret = nums2[l2]
+				l2++
+			}
+		} else if l1 < len(nums1) {
+			ret = nums1[l1]
+			l1++
+		} else if l2 < len(nums2) {
+			ret = nums2[l2]
+			l2++
+		}
+		k--
+	}
+	return float64(ret)
 }
